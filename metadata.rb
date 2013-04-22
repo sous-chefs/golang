fdb10f2f8ef189b08615527a800d47d3dc8d8610
Matthew Moore <matt@crowdmob.com>
2013-04-21 23:48:38 -0700

    Works with Amazon OpsWorks and lets users easily specify another install dir

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name             'chef-golang'
+name             'golang'
 maintainer       'Alexander Rozhnov'
 maintainer_email 'gnox73@gmail.com'
 license          'All rights reserved'
@@ -6,5 +6,5 @@ description      'Installs go programming language'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.1'
 
-recipe "chef-golang", "Installs go programing language."
-recipe "chef-golang::install", "Installs go programing language."
+recipe "golang", "Installs go programing language."
+recipe "golang::install", "Installs go programing language."
