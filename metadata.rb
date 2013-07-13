363bc494e1977c33ebdb39ed0f219fbfe583cfd0
Anton Kalyaev <anton.kalyaev@gmail.com>
2013-07-13 14:06:19 +0400

    use a proper license name

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'golang'
 maintainer       'Alexander Rozhnov'
 maintainer_email 'gnox73@gmail.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs go programming language'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.0'
