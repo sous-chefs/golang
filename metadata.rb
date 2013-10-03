a66f5911ee04ed431dd4d1ef53436958d31139a8
Rozhnov Alexandr <nox73@ya.ru>
2013-10-03 15:38:31 +0400

    release v1.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'gnox73@gmail.com'
 license          'Apache 2.0'
 description      'Installs go programming language'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.1'
+version          '1.1.2'
 
 recipe "golang", "Installs go programing language."
 recipe "golang::install", "Installs go programing language."
