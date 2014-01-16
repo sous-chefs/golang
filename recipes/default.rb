0d01e46c47175f513aa31f8ef5aa5877a9408afd
pseudomuto <david.muto@gmail.com>
2014-01-16 10:09:07 -0500

    adding owner and group attributes for gopath

--- recipes/default.rb
+++ recipes/default.rb
@@ -29,7 +29,7 @@ end
 
 remote_file File.join(Chef::Config[:file_cache_path], node['go']['filename']) do
   source node['go']['url']
-  owner "root"
+  owner 'root'
   mode 0644
   notifies :run, 'bash[install-golang]', :immediately
   not_if "#{node['go']['install_dir']}/go/bin/go version | grep #{node['go']['version']}"
@@ -38,23 +38,23 @@ end
 directory node['go']['gopath'] do
   action :create
   recursive true
-  owner "root"
-  group "root"
+  owner node['go']['owner']
+  group node['go']['group']
   mode 0755
 end
 
 directory node['go']['gobin'] do
   action :create
   recursive true
-  owner "root"
-  group "root"
+  owner 'root'
+  group 'root'
   mode 0755
 end
 
 template "/etc/profile.d/golang.sh" do
   source "golang.sh.erb"
-  owner "root"
-  group "root"
+  owner 'root'
+  group 'root'
   mode 0755
 end
 
