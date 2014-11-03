3560a022007e99c259c38d1754d9f13828f95e35
JD Stuart <jdstuart@users.noreply.github.com>
2014-11-03 15:25:04 -0800

    Update default_test.rb
    Update test to look for version 1.3

--- test/cookbooks/golang_test/files/default/tests/minitest/default_test.rb
+++ test/cookbooks/golang_test/files/default/tests/minitest/default_test.rb
@@ -5,7 +5,7 @@ describe 'golang::default' do
 
   it 'can execute go version' do
     go = shell_out("/usr/local/go/bin/go version")
-    assert_includes(go.stdout,'1.2')
+    assert_includes(go.stdout,'1.3')
   end
 
   it 'can run hello_world' do
