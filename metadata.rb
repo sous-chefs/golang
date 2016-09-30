e93c6ae04e149ce0a8bf546cdaa94d8acc7bf0a6
Michael Burns <michael@mirwin.net>
2016-09-29 22:14:19 -0700

    bump

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'gnox73@gmail.com'
 license          'Apache 2.0'
 description      'Installs go programming language'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.7.0'
+version          '1.7.1'
 
 recipe "golang", "Installs go programing language."
 recipe "golang::packages", "Installs go packages and SCM requirements."
