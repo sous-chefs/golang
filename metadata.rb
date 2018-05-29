3667f9d8158e7429c65a96612ac566adb2c918b7
Michael Burns <michael@mirwin.net>
2018-05-29 11:16:22 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'gnox73@gmail.com'
 license          'Apache-2.0'
 description      'Installs go programming language'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.0'
+version          '3.0.1'
 chef_version     '>= 12' if respond_to?(:chef_version)
 
 recipe 'chef-golang', 'Installs go programing language.'
