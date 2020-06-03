31a86a32b5722d955d9dd567cd62eeb3ef4ea929
Jeff Byrnes <thejeffbyrnes@gmail.com>
2020-06-03 15:26:36 -0400

    test: Add simple unit tests

---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook:: golang
+# Spec:: default
+#
+# Copyright:: 2020, Jeff Byrnes
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy
+# of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+
+require 'spec_helper'
+
+describe 'golang::default' do
+  context 'When all attributes are default, on Ubuntu 20.04' do
+    platform 'ubuntu', '20.04'
+
+    before do
+      stub_command('/usr/local/go/bin/go version | grep "go1.14.4 "').and_return false
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+end
